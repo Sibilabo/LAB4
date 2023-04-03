d0f392b23c1de53022df442a4c1d12f04259627b
Sonia Gwizdek <sonia.gwizdek95@gmail.com>
2023-04-03 22:36:03 +0200

    script.sh

--- script.sh
+++ script.sh
@@ -4,3 +4,16 @@
 if [ "$1" = "--date" ]; then
   date
 fi
+
+#skrypt tworzy 100 plików o rozszerzeniu txt, zapisuje w nich ich nazwę, nazwę skryptuktóry je stworzył oraz datę utworzenia
+
+nazwa_skryptu=$(basename "$0")
+data=$(date)
+if [ "$1" == "--logs" ]; then
+  for i in {1..100}; do
+  touch log$i.txt;
+  echo $data > log$i.txt
+  echo "Nazwa pliku:" log$i.txt >> log$i.txt
+  echo "Nazwa skryptu:" $nazwa_skryptu >> log$i.txt
+  done
+fi
