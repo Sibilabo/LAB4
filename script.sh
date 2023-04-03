8730a0c9e844bdb9a4300f2e9494f5e1e0e11da2
Sonia Gwizdek <sonia.gwizdek95@gmail.com>
2023-04-03 22:41:34 +0200

    script.sh

--- script.sh
+++ script.sh
@@ -17,3 +17,19 @@ if [ "$1" == "--logs" ]; then
   echo "Nazwa skryptu:" $nazwa_skryptu >> log$i.txt
   done
 fi
+
+#skrypt tworzący 30 plików
+
+data=$(date)
+nazwa_skryptu=$(basename "$0")
+
+if [ "$1" = "--logs" ]; then
+  if [ "$2" -gt 0 ] 2>/dev/null; then
+    for ((i=1; i<=$2; i++)); do
+      touch log$i.txt
+      echo $data > log$i.txt
+      echo log$i.txt >> log$i.txt
+      echo $nazwa_skryptu >> log$i.txt
+    done
+  fi
+fi
